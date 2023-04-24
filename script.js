d19d0d5e14e65660e22702b9cff34ff6ba351726
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-04-24 16:02:03 +0100

    function generatePassword first steps added

--- script.js
+++ script.js
@@ -10,5 +10,17 @@ function writePassword() {
 
 }
 
+function generatePassword () {
+  
+  const specialCharacters = "!£$%^&*()-+={}[]`:;@'~#|\<>?,./";
+  const numbers = "0123456789";
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+
+
+
+  return ("serul;okopl[")
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
